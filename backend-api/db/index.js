0c4fb6dcbaa3c84062b7db046da1c274adee5e99
SJ <sj.jega2@gmail.com>
2021-11-05 00:08:18 -0400

    db code fix

--- backend-api/db/index.js
+++ backend-api/db/index.js
@@ -24,13 +24,5 @@ const client = new Client({
 
 client.connect();
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
-
 
 module.exports = client;
