fc4626fa3b00d3fec6bc4a3a0911dd78f178822e
SJ <sj.jega2@gmail.com>
2021-11-08 20:39:14 -0500

    static asset path change

--- backend-api/app.js
+++ backend-api/app.js
@@ -22,7 +22,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(__dirname+"/frontend-client/build"))
 app.use(cors({
   origins: ['http://localhost:3000/']
 }));
